0a81960bfb783190968a2fade6d3b620b7585a83
roiprez <roiprezl@gmail.com>
2016-07-16 18:05:44 +0200

    Adding 1 to 10 numbers String to the NumbersActivity file Adding also some logs to make sure everything works properly

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -25,6 +25,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
