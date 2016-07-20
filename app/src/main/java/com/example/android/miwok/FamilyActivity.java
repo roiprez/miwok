566091b84fb7b923150df49db9e007f36086aa18
roiprez <roiprezl@gmail.com>
2016-07-20 23:01:42 +0200

    NumbersActivity XML renamed to CategoriesActivity

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -25,7 +25,7 @@ public class FamilyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.activity_categories);
 
         ArrayList<Word> words = new ArrayList<>();
 
