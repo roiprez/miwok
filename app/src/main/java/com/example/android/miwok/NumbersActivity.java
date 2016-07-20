c326f3c302ca61c3f28df8f5dfa4aa76e00bcaf0
roiprez <roiprezl@gmail.com>
2016-07-20 22:57:41 +0200

    Every java activity is set to the same as the changes in the numbers activity But is not ready yet the translations. Each category's XML was deleted, now all of them refer to the NumbersActivity XML

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,10 +17,6 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout.LayoutParams;
 import android.widget.ListView;
 import java.util.ArrayList;
 
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -17,12 +17,31 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phrases);
+        setContentView(R.layout.activity_numbers);
+
+        ArrayList<Word> words = new ArrayList<>();
+
+        words.add(new Word("black", "lutti"));
+        words.add(new Word("yellow", "otiiko"));
+        words.add(new Word("blue", "tollokosu"));
+
+
+
+        //Creating an ArrayAdapter which creates an array that fits the layout of a view
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+
+        //Creating the ListView object from the ListView XML
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        //Set an adapter for the ListView
+        listView.setAdapter(itemsAdapter);
     }
 }
